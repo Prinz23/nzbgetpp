0597cd3e9624ec4bd12b37f8ff43d3a059d3b0e0
Prinz23 <Prinz2311@gmail.com>
2019-05-21 23:19:59 +0200

    upgrade rarfile lib

--- rarfile/rarfile.py
+++ rarfile/rarfile.py
@@ -140,8 +140,12 @@ except ImportError:
     _have_crypto = 0
 
 try:
-    from pyblake2 import blake2s
-    _have_blake2 = True
+    try:
+        from hashlib import blake2s
+        _have_blake2 = True
+    except ImportError:
+        from pyblake2 import blake2s
+        _have_blake2 = True
 except ImportError:
     _have_blake2 = False
 
@@ -166,6 +170,9 @@ else:  # pragma: no cover
     unicode = str
     _byte_code = int   # noqa
 
+# don't break 2.6 completely
+if sys.hexversion < 0x2070000:
+    memoryview = lambda x: x  # noqa
 
 __version__ = '3.0'
 
@@ -210,6 +217,12 @@ ALT_OPEN_ARGS = ('-x', '--to-stdout', '-f')
 ALT_EXTRACT_ARGS = ('-x', '-f')
 ALT_TEST_ARGS = ('-t', '-f')
 ALT_CHECK_ARGS = ('--help',)
+
+#ALT_TOOL = 'unar'
+#ALT_OPEN_ARGS = ('-o', '-')
+#ALT_EXTRACT_ARGS = ()
+#ALT_TEST_ARGS = ('-test',) # does not work
+#ALT_CHECK_ARGS = ('-v',)
 
 #: whether to speed up decompression by using tmp archive
 USE_EXTRACT_HACK = 1
@@ -380,8 +393,8 @@ UTC = timezone(timedelta(0), 'UTC')
 BSIZE = 32 * 1024
 
 def _get_rar_version(xfile):
-    '''Check quickly whether file is rar archive.
-    '''
+    """Check quickly whether file is rar archive.
+    """
     with XFile(xfile) as fd:
         buf = fd.read(len(RAR5_ID))
     if buf.startswith(RAR_ID):
@@ -395,8 +408,8 @@ def _get_rar_version(xfile):
 ##
 
 def is_rarfile(xfile):
-    '''Check quickly whether file is rar archive.
-    '''
+    """Check quickly whether file is rar archive.
+    """
     return _get_rar_version(xfile) > 0
 
 class Error(Exception):
@@ -473,7 +486,7 @@ class RarCannotExec(RarExecError):
 
 
 class RarInfo(object):
-    r'''An entry in rar archive.
+    r"""An entry in rar archive.
 
     RAR3 extended timestamps are :class:`datetime.datetime` objects without timezone.
     RAR5 extended timestamps are :class:`datetime.datetime` objects with UTC timezone.
@@ -566,7 +579,7 @@ class RarInfo(object):
         volume_file
             Volume file name, where file starts.
 
-    '''
+    """
 
     # zipfile-compatible fields
     filename = None
@@ -616,8 +629,8 @@ class RarInfo(object):
 
 
 class RarFile(object):
-    '''Parse RAR structure, provide access to files in archive.
-    '''
+    """Parse RAR structure, provide access to files in archive.
+    """
 
     #: Archive comment.  Unicode string or None.
     comment = None
@@ -662,13 +675,16 @@ class RarFile(object):
         self._parse()
 
     def __enter__(self):
+        """Open context."""
         return self
 
     def __exit__(self, typ, value, traceback):
+        """Exit context"""
         self.close()
 
     def setpassword(self, password):
-        '''Sets the password to use when extracting.'''
+        """Sets the password to use when extracting.
+        """
         self._password = password
         if self._file_parser:
             if self._file_parser.has_header_encryption():
@@ -679,33 +695,35 @@ class RarFile(object):
             self._file_parser.setpassword(self._password)
 
     def needs_password(self):
-        '''Returns True if any archive entries require password for extraction.'''
+        """Returns True if any archive entries require password for extraction.
+        """
         return self._file_parser.needs_password()
 
     def namelist(self):
-        '''Return list of filenames in archive.'''
+        """Return list of filenames in archive.
+        """
         return [f.filename for f in self.infolist()]
 
     def infolist(self):
-        '''Return RarInfo objects for all files/directories in archive.'''
+        """Return RarInfo objects for all files/directories in archive.
+        """
         return self._file_parser.infolist()
 
     def volumelist(self):
-        '''Returns filenames of archive volumes.
+        """Returns filenames of archive volumes.
 
         In case of single-volume archive, the list contains
         just the name of main archive file.
-        '''
+        """
         return self._file_parser.volumelist()
 
     def getinfo(self, fname):
-        '''Return RarInfo for file.
-        '''
+        """Return RarInfo for file.
+        """
         return self._file_parser.getinfo(fname)
 
     def open(self, fname, mode='r', psw=None):
-        '''Returns file-like object (:class:`RarExtFile`),
-        from where the data can be read.
+        """Returns file-like object (:class:`RarExtFile`) from where the data can be read.
 
         The object implements :class:`io.RawIOBase` interface, so it can
         be further wrapped with :class:`io.BufferedReader`
@@ -726,7 +744,7 @@ class RarFile(object):
                 must be 'r'
             psw
                 password to use for extracting.
-        '''
+        """
 
         if mode != 'r':
             raise NotImplementedError("RarFile.open() supports only mode=r")
@@ -823,8 +841,7 @@ class RarFile(object):
             check_returncode(p, output)
 
     def strerror(self):
-        """Return error string if parsing failed,
-        or None if no problems.
+        """Return error string if parsing failed or None if no problems.
         """
         if not self._file_parser:
             return "Not a RAR file"
@@ -935,9 +952,14 @@ class CommonParser(object):
         """
         return self._info_list
 
-    def getinfo(self, fname):
+    def getinfo(self, member):
         """Return RarInfo for filename
         """
+        if isinstance(member, RarInfo):
+            fname = member.filename
+        else:
+            fname = member
+
         # accept both ways here
         if PATH_SEP == '/':
             fname2 = fname.replace("\\", "/")
@@ -1951,6 +1973,8 @@ class RarExtFile(RawIOBase):
     name = None
 
     def __init__(self, parser, inf):
+        """Open archive entry.
+        """
         super(RarExtFile, self).__init__()
 
         # standard io.* properties
@@ -2100,7 +2124,8 @@ class RarExtFile(RawIOBase):
     def writable(self):
         """Returns False.
 
-        Writing is not supported."""
+        Writing is not supported.
+        """
         return False
 
     def seekable(self):
@@ -2509,6 +2534,53 @@ class Blake2SP(object):
         """Hexadecimal digest."""
         return tohex(self.digest())
 
+
+class Rar3Sha1(object):
+    """Bug-compat for SHA1
+    """
+    digest_size = 20
+    block_size = 64
+
+    _BLK_BE = struct.Struct(b'>16L')
+    _BLK_LE = struct.Struct(b'<16L')
+
+    __slots__ = ('_nbytes', '_md', '_rarbug')
+
+    def __init__(self, data=b'', rarbug=False):
+        self._md = sha1()
+        self._nbytes = 0
+        self._rarbug = rarbug
+        self.update(data)
+
+    def update(self, data):
+        """Process more data."""
+        self._md.update(data)
+        bufpos = self._nbytes & 63
+        self._nbytes += len(data)
+
+        if self._rarbug and len(data) > 64:
+            dpos = self.block_size - bufpos
+            while dpos + self.block_size <= len(data):
+                self._corrupt(data, dpos)
+                dpos += self.block_size
+
+    def digest(self):
+        """Return final state."""
+        return self._md.digest()
+
+    def hexdigest(self):
+        """Return final state as hex string."""
+        return self._md.hexdigest()
+
+    def _corrupt(self, data, dpos):
+        """Corruption from SHA1 core."""
+        ws = list(self._BLK_BE.unpack_from(data, dpos))
+        for t in range(16, 80):
+            tmp = ws[(t - 3) & 15] ^ ws[(t - 8) & 15] ^ ws[(t - 14) & 15] ^ ws[(t - 16) & 15]
+            ws[t & 15] = ((tmp << 1) | (tmp >> (32 - 1))) & 0xFFFFFFFF
+        self._BLK_LE.pack_into(data, dpos, *ws)
+
+
 ##
 ## Utility functions
 ##
@@ -2656,7 +2728,7 @@ def _parse_xtime(flag, data, pos, basetime=None):
 def is_filelike(obj):
     """Filename or file object?
     """
-    if isinstance(obj, str) or isinstance(obj, unicode):
+    if isinstance(obj, (bytes, unicode)):
         return False
     res = True
     for a in ('read', 'tell', 'seek'):
@@ -2670,13 +2742,14 @@ def rar3_s2k(psw, salt):
     """
     if not isinstance(psw, unicode):
         psw = psw.decode('utf8')
-    seed = psw.encode('utf-16le') + salt
+    seed = bytearray(psw.encode('utf-16le') + salt)
+    h = Rar3Sha1(rarbug=True)
     iv = EMPTY
-    h = sha1()
     for i in range(16):
         for j in range(0x4000):
             cnt = S_LONG.pack(i * 0x4000 + j)
-            h.update(seed + cnt[:3])
+            h.update(seed)
+            h.update(cnt[:3])
             if j == 0:
                 iv += h.digest()[19:20]
     key_be = h.digest()[:16]
@@ -2798,6 +2871,8 @@ def custom_popen(cmd):
     except OSError as ex:
         if ex.errno == errno.ENOENT:
             raise RarCannotExec("Unrar not installed? (rarfile.UNRAR_TOOL=%r)" % UNRAR_TOOL)
+        if ex.errno == errno.EACCES or ex.errno == errno.EPERM:
+            raise RarCannotExec("Cannot execute unrar (rarfile.UNRAR_TOOL=%r)" % UNRAR_TOOL)
         raise
     return p
 
@@ -2856,6 +2931,7 @@ def hmac_sha256(key, data):
     return HMAC(key, data, sha256).digest()
 
 def membuf_tempfile(memfile):
+    """Write in-memory file object to real file."""
     memfile.seek(0, 0)
 
     tmpfd, tmpname = mkstemp(suffix='.rar')
@@ -2875,6 +2951,8 @@ def membuf_tempfile(memfile):
     return tmpname
 
 class XTempFile(object):
+    """Real file for archive.
+    """
     __slots__ = ('_tmpfile', '_filename')
 
     def __init__(self, rarfile):
@@ -2926,7 +3004,8 @@ def _check_unrar_tool():
             TEST_ARGS = ALT_TEST_ARGS
         except RarCannotExec:
             # no usable tool, only uncompressed archives work
-            pass
+            return False
+    return True
 
 _check_unrar_tool()
 
